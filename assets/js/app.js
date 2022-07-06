11213a7631197164ed50408067bd7e2994a68c56
Diego Chávez Martínez <99066735+DiegoChavezMtz@users.noreply.github.com>
2022-07-05 22:35:56 -0500

    FIrst commit of Quiniela

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+import {participantes} from './data.js';
+
+let tablero= document.querySelector('.tablero');
+
+let group = document.createElement("h2");
+
+window.addEventListener('load',()=>{
+
+    tablero.appendChild(group);
+    
+    
+
+})
---
+++ assets/js/data.js
@@ -0,0 +1,148 @@
+export let participantes = {
+    grupoA: {
+        Qatar:{
+            nombre:'Qatar',
+            bandera: './../img/flags/Catar-Bandera-Asia.png'
+        },
+        Ecuador:{
+            nombre:'Ecuador',
+            bandera:'./../img/flags/Ecuador-Bandera-America.png'
+        },
+        Senegal:{
+            nombre:'Senegal',
+            bandera:'./../img/flags/Senegal-Bandera-Africa.png'
+        },
+        Holanda:{
+            nombre: 'Holanda',
+            bandera: './../img/flags/Países-Bajos-Bandera-Europa.png'
+        }
+
+    },
+    grupoB: {
+        Inglaterra:{
+            nombre:'Inglaterra',
+            bandera: './../img/flags/Inglaterra-Bandera-Europa.png'
+        },
+        Iran:{
+            nombre:'RI de Irán',
+            bandera:'./../img/flags/Irán-Bandera-Asia.png'
+        },
+        EU:{
+            nombre:'Estados Unidos',
+            bandera:'./../img/flags/EEUU-Bandera-America.png'
+        },
+        Gales:{
+            nombre: 'Gales',
+            bandera: './../img/flags/Gales-Bandera-Europa.png'
+        }
+    } ,
+    grupoC: {
+        Argentina:{
+            nombre:'Argentina',
+            bandera: './../img/flags/Argentina-Bandera-America.png'
+        },
+        Arabia:{
+            nombre:'Arabia Saudí',
+            bandera:'./../img/flags/Arabia-Saudita-Bandera-Asia.png'
+        },
+        Mexico:{
+            nombre:'México',
+            bandera:'./../img/flags/México-Bandera-America.png'
+        },
+        Polonia:{
+            nombre: 'Polonia',
+            bandera: './../img/flags/Polonia-Bandera-Europa.png'
+        }
+    },
+    grupoD: {
+        Francia:{
+            nombre:'Francia',
+            bandera: './../img/flags/Francia-Bandera-Europa.png'
+        },
+        Australia:{
+            nombre:'Australia',
+            bandera:'./../img/flags/Australia-Bandera-Oceanía.png'
+        },
+        Dinamarca:{
+            nombre:'Dinamarca',
+            bandera:'./../img/flags/Dinamarca-Bandera-Europa.png'
+        },
+        Tunez:{
+            nombre: 'Túnez',
+            bandera: './../img/flags/Túnez-Bandera-Africa.png'
+        }
+    },
+    grupoE: {
+        Espana:{
+            nombre:'España',
+            bandera: './../img/flags/España-Bandera-Europa.png'
+        },
+        CostaR:{
+            nombre:'Costa Rica',
+            bandera:'./../img/flags/Costa-Rica-Bandera-America.png'
+        },
+        Alemania:{
+            nombre:'Alemania',
+            bandera:'./../img/flags/Alemania-Bandera-Europa.png'
+        },
+        Japon:{
+            nombre: 'Japón',
+            bandera: './../img/flags/Japón-Bandera-Asia.png'
+        }
+    },
+    grupoF: {
+        Belglica:{
+            nombre:'Bélgica',
+            bandera: './../img/flags/Bélgica-Bandera-Europa.png'
+        },
+        Canada:{
+            nombre:'Canadá',
+            bandera:'./../img/flags/Canadá-Bandera-America.png'
+        },
+        Marruecos:{
+            nombre:'Marruecos',
+            bandera:'./../img/flags/Marruecos-Bandera-Africa.png'
+        },
+        Croacia:{
+            nombre: 'Croacia',
+            bandera: './../img/flags/Croacia-Bandera-Europa.png'
+        }
+    },
+    grupoG: {
+        Brasil:{
+            nombre:'Brasil',
+            bandera: './../img/flags/Brasil-Bandera-America.png'
+        },
+        Serbia:{
+            nombre:'Serbia',
+            bandera:'./../img/flags/Serbia-Bandera-Europa.png'
+        },
+        Suiza:{
+            nombre:'Suiza',
+            bandera:'./../img/flags/Suiza-Bandera-Europa.png'
+        },
+        Camerun:{
+            nombre: 'Camerún',
+            bandera: './../img/flags/Camerún-Bandera-Africa.png'
+        }
+    },
+    grupoH: {
+        Portugal:{
+            nombre:'Portugal',
+            bandera: './../img/flags/Portugal-Bandera-Europa.png'
+        },
+        Ghana:{
+            nombre:'Ghana',
+            bandera:'./../img/flags/Ghana-Bandera-Africa.png'
+        },
+        Uruguay:{
+            nombre:'Uruguay',
+            bandera:'./../img/flags/Uruguay-Bandera-America.png'
+        },
+        Corea:{
+            nombre: 'Republica de Corea',
+            bandera: './../img/flags/Corea-del-Sur-Bandera-Asia.png'
+        }
+    },
+    
+}
